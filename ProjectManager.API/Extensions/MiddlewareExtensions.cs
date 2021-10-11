ad31a4e3328e362e319c375e3694d2f61bbc06a8
YaroslavKust <79914649+YaroslavKust@users.noreply.github.com>
2021-10-11 23:30:08 +0300

    add global exception handler

---
+++ ProjectManager.API/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+
+namespace ProjectManager.API.Extensions
+{
+    public static class MiddlewareExtensions
+    {
+        public static void ConfigureExceptionsHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+                var result = JsonConvert.SerializeObject(new { Message = "Internal server error" });
+                await context.Response.WriteAsync(result);
+            }));
+        }
+    }
+}
