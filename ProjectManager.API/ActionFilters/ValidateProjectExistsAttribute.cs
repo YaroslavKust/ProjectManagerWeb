f6787dd2e3b79618d8d360905e31dd371901da88
YaroslavKust <79914649+YaroslavKust@users.noreply.github.com>
2021-10-11 23:29:24 +0300

    Add action filters

---
+++ ProjectManager.API/ActionFilters/ValidateProjectExistsAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ProjectManager.DAL.UnitOfWorks;
+using ProjectManager.Entities.Models;
+
+namespace ProjectManager.API.ActionFilters
+{
+    public class ValidateProjectExistsAttribute : IAsyncActionFilter
+    {
+        private IUnitOfWork _unit;
+
+        public ValidateProjectExistsAttribute(IUnitOfWork unit)
+        {
+            _unit = unit;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var userId = Int32.Parse(context.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            var projectId = (int)context.ActionArguments["projectId"];
+            var project = await _unit.Projects.GetProjectAsync(userId, projectId);
+
+            if (project is null)
+            {
+                context.Result = new NotFoundResult();
+            }
+            else
+            {
+                await next();
+            }
+            
+        }
+    }
+}
---
+++ ProjectManager.API/ActionFilters/ValidateTaskExistsAttribute.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ProjectManager.DAL.UnitOfWorks;
+using System.Threading.Tasks;
+
+namespace ProjectManager.API.ActionFilters
+{
+    public class ValidateTaskExistsAttribute: IAsyncActionFilter
+    {
+        private IUnitOfWork _unit;
+
+        public ValidateTaskExistsAttribute(IUnitOfWork unit)
+        {
+            _unit = unit;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var projectId = (int)context.ActionArguments["projectId"];
+            var id = (int)context.ActionArguments["id"];
+
+            var task = await _unit.Tasks.GetTaskAsync(projectId, id);
+
+            if (task is null)
+            {
+                context.Result = new NotFoundResult();
+            }
+            else
+            {
+                context.HttpContext.Items.Add("SelectedTask", task);
+                await next();
+            }
+
+            
+        }
+    }
+}
