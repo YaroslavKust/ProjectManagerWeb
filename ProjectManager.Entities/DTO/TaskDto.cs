b07b8c2cd2db08a7ff28bef9a7ab286ae64e6f57
YaroslavKust <79914649+YaroslavKust@users.noreply.github.com>
2021-10-16 15:12:26 +0300

    update TaskDto class

--- ProjectManager.Entities/DTO/TaskDto.cs
+++ ProjectManager.Entities/DTO/TaskDto.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public string Description { get; set; }
         public int ProgressInPercents { get; set; }
+        public int ProjectId { get; set; }
     }
 }
